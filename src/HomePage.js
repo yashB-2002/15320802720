2f5e4d4709be6d2f40e24d68e19a0bafff99a46e
Yash Bhardwaj <86493014+yashB-2002@users.noreply.github.com>
2023-07-28 17:11:00 +0530

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -8,7 +8,7 @@ function HomePage() {
         method: "GET",
         headers: {
           Authentication:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTA1MzU1MzUsImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiZTA4YjJjN2QtZjA5YS00MzE3LWEwYjYtNWRjZjFiMzkwZGI1Iiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IjE1MzIwODAyNzIwIn0.zFx2rjo7ontSgrSHdAI0QZ2U4pVjYzbjzfUqiph5rV4",
+            "Bearer {token},
         },
       });
       const data = await result.json();
