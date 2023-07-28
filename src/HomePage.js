d61b820b7c3a538af9675606f1a8e0ee3876e11f
Yash Bhardwaj <86493014+yashB-2002@users.noreply.github.com>
2023-07-28 19:34:01 +0530

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,20 +4,37 @@ import { apidata } from "./apidata";
 function HomePage() {
   const [res, setRes] = useState(apidata);
   console.log("before sort" + apidata);
-
+   function authorize() {
+      const response = await fetch("http://20.244.56.144/train/auth", {
+      method: "POST",
+      body: JSON.stringify({
+        companyName: process.env.name,
+        clientID: process.env.clientid,
+        clientSecret: process.env.clientsecret,
+        ownerName: process.env.username,
+        ownerEmail: process.env.name,
+        rollNo: "15320802720",
+      }),
+    });
+    const data = await response.json();
+    return data;
+  }
   useEffect(() => {
-    const getData = async () => {
-      const result = await fetch("http://20.244.56.144/train/trains", {
-        method: "GET",
+    const fetchDetails = async () => {
+      let token = localStorage.getItem("token");
+      if (!token) {
+        const data = await authorize();
+        token = data.access_token;
+        localStorage.setItem("token", token);
+      }
+      const response = await fetch("http://20.244.56.144/train/trains", {
         headers: {
-          Authentication: "Bearer {token}",
+          Authorization: "Bearer " + token,
         },
       });
-      const data = await result.json();
-      console.log(data);
-      setRes(data);
+      const data = await response.json();
     };
-    getData();
+    fetchDetails();
   }, []);
   return (
     <div>
