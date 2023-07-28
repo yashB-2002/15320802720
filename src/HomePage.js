4a73d7b0fe42d287801dcdd12375a05d63302973
Yash Bhardwaj <86493014+yashB-2002@users.noreply.github.com>
2023-07-28 18:11:22 +0530

    Updated files

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 import TableComp from "./TableComp";
+import { apidata } from "./apidata";
 function HomePage() {
-  const [res, setRes] = useState([]);
+  const [res, setRes] = useState(apidata);
+  console.log("before sort" + apidata);
+
   useEffect(() => {
     const getData = async () => {
       const result = await fetch("http://20.244.56.144/train/trains", {
         method: "GET",
         headers: {
-          Authentication:
-            "Bearer {token},
+          Authentication: "Bearer {token}",
         },
       });
       const data = await result.json();
@@ -19,7 +21,7 @@ function HomePage() {
   }, []);
   return (
     <div>
-      <TableComp res={res} />
+      <TableComp />
     </div>
   );
 }
